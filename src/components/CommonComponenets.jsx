04a4f04699ec154e87307b061c40ae50a4ef849a
drshnn <darshansu156@gmail.com>
2023-04-09 20:03:01 +0530

    added first componenet dropdown

--- src/components/CommonComponenets.jsx
+++ src/components/CommonComponenets.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export const Dropdown = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
+  const listItemHoverStyle = "hover:bg-gray-200 rounded-md p-2 cursor-pointer";
   return (
     <div className="dropdown relative flex flex-col items-center">
       <button
@@ -11,10 +12,10 @@ export const Dropdown = () => {
         dropdown
       </button>
       {toggleDropdown && (
-        <ul className="absolute top-12  flex flex-col py-3 px-2 w-36 bg-white rounded-md border-2 border-gray-200">
-          <li>first item</li>
-          <li>second item</li>
-          <li>third item</li>
+        <ul className="absolute top-12 flex flex-col py-3 px-2 w-36 bg-white rounded-md border-2 border-gray-200 ">
+          <li className={listItemHoverStyle}>first item</li>
+          <li className={listItemHoverStyle}>second item</li>
+          <li className={listItemHoverStyle}>third item</li>
         </ul>
       )}
     </div>
